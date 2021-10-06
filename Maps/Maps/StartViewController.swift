f04e45b3ef7a84214b01da77c0179e8bc83ebf50
Marina Trufanova <trufanovamyu@gmail.com>
2021-10-06 14:56:46 +0300

    create project

---
+++ Maps/Maps/StartViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Maps
+//
+//  Created by msc on 06.10.2021.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
