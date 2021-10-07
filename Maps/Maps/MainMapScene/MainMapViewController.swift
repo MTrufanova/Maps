b3904443b53f905b156f1c282ca8504779bcc199
Marina Trufanova <trufanovamyu@gmail.com>
2021-10-07 13:01:27 +0300

    check map example

--- Maps/Maps/MainMapScene/MainMapViewController.swift
+++ Maps/Maps/MainMapScene/MainMapViewController.swift
@@ -6,16 +6,39 @@
 //
 
 import UIKit
+import YandexMapsMobile
 
 class MainMapViewController: UIViewController {
 
+    private lazy var mapView: YMKMapView = {
+        let mapView = YMKMapView()
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        return mapView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .blue
+        createMapView()
+        setupLayout()
     }
     
+    private func createMapView() {
+        mapView.mapWindow.map.move(
+            with: YMKCameraPosition.init(target: YMKPoint(latitude: 55.751574, longitude: 37.573856), zoom: 15, azimuth: 0, tilt: 0),
+            animationType: YMKAnimation(type: YMKAnimationType.smooth, duration: 5),
+            cameraCallback: nil)
+    }
 
+    private func setupLayout() {
+        view.addSubview(mapView)
+
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
 
 
 }
